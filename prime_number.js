c10c0563a99b04be3a1f57d5ab25d04a5eec0f39
sushant2518 <sushantnarvekar3118@gmail.com>
2022-11-03 07:34:48 +0530

    check the code and correct

---
+++ prime_number.js
@@ -0,0 +1,14 @@
+    let num=13;
+    let counter=0;
+    for(let i=0; i>num; i--){
+        if(num%i==0){
+        counter++;
+        }
+    }
+    if(counter==2){
+        console.log("Yes");
+    }
+    else{
+        console.log("No");
+    }
+
